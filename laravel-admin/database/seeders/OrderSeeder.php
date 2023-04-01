6a3dbd8dd4a85b03c3e94422d7d9f80d8402af72
alexsumoski <alexsumoski@gmail.com>
2023-04-01 13:49:44 -0400

    Order item migration issue fix

--- laravel-admin/database/seeders/OrderSeeder.php
+++ laravel-admin/database/seeders/OrderSeeder.php
@@ -4,19 +4,21 @@ namespace Database\Seeders;
 
 use App\Models\Order;
 use App\Models\OrderItem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         Order::factory(30)->create()
-            ->each(fn(Order $order) => OrderItem::factory(random_int(1, 5))
-                ->create(['order_id' => $order->id]));
+            ->each(function(Order $order) {
+                OrderItem::factory(random_int(1, 5))->create([
+                    'order_id' => $order->id
+                ]);
+            });
     }
 }
