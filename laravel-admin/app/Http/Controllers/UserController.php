cfe2c1c7feb4be97e31c1d2e809a4a4b64ae13ff
alexsumoski <alexsumoski@gmail.com>
2023-04-02 10:27:38 -0400

    Role authorization gate

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
@@ -13,11 +14,15 @@ class UserController extends Controller
 {
     public function index()
     {
+        $this->authorize('view','users');
+
         return UserResource::collection(User::paginate());
     }
 
     public function store(Request $request)
     {
+        $this->authorize('edit','users');
+
         $user = User::create(
             $request->only('first_name', 'last_name', 'email', 'role_id')
             + ['password' => Hash::make(1234)]
@@ -28,11 +33,15 @@ class UserController extends Controller
 
     public function show(string $id)
     {
+        $this->authorize('view','users');
+
         return new UserResource(User::with('role')->find($id));
     }
 
     public function update(UserUpdateRequest $request, string $id)
     {
+        $this->authorize('edit','users');
+
         $user = User::find($id);
 
         $user->update($request->only('first_name', 'last_name', 'email'));
@@ -42,6 +51,8 @@ class UserController extends Controller
 
     public function destroy(string $id)
     {
+        $this->authorize('edit','users');
+
         User::destroy($id);
 
         return response(null, Response::HTTP_NO_CONTENT); 
--- laravel-admin/app/Providers/AppServiceProvider.php
+++ laravel-admin/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        Gate::define('view', function(User $user, $model) {
+            return $user->hasAccess("view_{$model}") || $user->hasAccess("edit{$model}");
+        });
+
+        Gate::define('edit', fn (User $user, $model) => $user->hasAccess("edit{$model}"));
     }
 }
